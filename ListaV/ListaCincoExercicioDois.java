9fb311ef5110b1fb160781382780d8b9483befd6
João Victor Carvalho Dimas de Oliveira <130412515+Dimasjvco@users.noreply.github.com>
2023-06-01 21:17:38 -0300

    Update ListaCincoExercicioDois.java

--- ListaV/ListaCincoExercicioDois.java
+++ ListaV/ListaCincoExercicioDois.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PrecoProduto {
+public class ListaCincoExercicioDois {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
